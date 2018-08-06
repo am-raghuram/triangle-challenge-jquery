a2a12aecec2aa7fc96d8030816531c49e672ab30
Raghuram A M <am.raghuram@gmail.com>
2018-08-06 23:32:22 +0530

    Code Refactoring

--- static/application.js
+++ static/application.js
@@ -4,25 +4,27 @@
 $(document).ready(function () {
     "use strict";
     console.log("ready!");
- 
 
+    //onSubmit Action
     $('form#triangle-input').submit(function (event) {
         var invalid = false;
-        console.log("Checking..")
+        //Reset fields to clear any previous value
         $('#type').empty();
         $('#result').hide();
+
+        //Form validation for mandatory fields using TradeShift UI Error message
         $('form#triangle-input').find('.ts-error').removeClass('ts-error');
         $('form#triangle-input').find('.ts-errors').hide();
-        $('form#triangle-input').find('input').each(function(){
-          //  console.log($(this).val());
-            if($(this).prop('required') && $(this).val().trim() ==""){
+        $('form#triangle-input').find('input').each(function () {
+            //  console.log($(this).val());
+            if ($(this).prop('required') && $(this).val().trim() == "") {
                 $(this).parent().addClass("ts-error");
                 $(this).parent().siblings(".ts-errors").show();
                 console.log("Required")
-            invalid = true;
-            } 
+                invalid = true;
+            }
         });
-        if(invalid){
+        if (invalid) {
             return false;
         }
 
@@ -30,10 +32,7 @@ $(document).ready(function () {
         var y = parseInt($('input#side2').val());
         var z = parseInt($('input#side3').val());
 
-    
-
-
-
+        //Sorting the sides and assigning in descending order of length to perform triangle validation
         var sorted = [x, y, z].sort(function (a, b) {
             return b - a;
         });
@@ -44,14 +43,20 @@ $(document).ready(function () {
 
         console.log(x + "---" + y + "---" + z);
 
-
+        //Triangle Object
+        /**********
+         * Side1, Side2, Side 3  
+         * type() - Returns type of triangle, validitates and returns if invalid
+         * height() - Used to determine the points for plotting the triangle
+         * short() - Used to determine the points for plotting the triangle
+         ***********/
         var triangle = {
-			side1: parseFloat(x),
+            side1: parseFloat(x),
             side2: parseFloat(y),
-            side3: parseFloat(z),	
+            side3: parseFloat(z),
             type: function () {
 
-                  if ((this.side1 > 0 && this.side2 > 0 && this.side3 > 0) && (this.side1 <= (this.side2 + this.side3))){
+                if ((this.side1 > 0 && this.side2 > 0 && this.side3 > 0) && (this.side1 <= (this.side2 + this.side3))) {
 
                     if (this.side1 === this.side2 && this.side2 === this.side3 && this.side3 === this.side1) {
                         return "Equilateral";
@@ -82,7 +87,7 @@ $(document).ready(function () {
             }
         };
 
-       
+
         console.log(triangle.type());
         console.log(triangle.height());
         console.log(triangle.short());
@@ -90,19 +95,19 @@ $(document).ready(function () {
         $('#result').show();
         $('#type').text(triangle.type());
 
-        var x1 =  x;
+        var x1 = x;
         var x2 = triangle.short();
         var y2 = triangle.height();
 
-        console.log("SVG-->"+x1+"--"+x2+"--"+y2);
- 
-         if ($('#type').text() !== "Side lengths are invalid to form a triangle !") {
-             $('#picture').show();
-                                         //point1    //point2     //point3
-             $('polygon').attr("points", '0,0 ' + x1 + ",0 " + x2 + "," + y2);
-         } else {
-             $('#picture').hide();
-         }
+        console.log("SVG-->" + x1 + "--" + x2 + "--" + y2);
+
+        if ($('#type').text() !== "Side lengths are invalid to form a triangle !") {
+            $('#picture').show();
+            //point1    //point2     //point3
+            $('polygon').attr("points", '0,0 ' + x1 + ",0 " + x2 + "," + y2);
+        } else {
+            $('#picture').hide();
+        }
 
     });
 
